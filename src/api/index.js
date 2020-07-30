22b4f6a01fce5b95a30725262246526026b3e24a
izqalan <izqalan@gmail.com>
2020-07-31 05:46:02 +0800

    feature: /mini - grouped images

--- src/api/index.js
+++ src/api/index.js
@@ -2,8 +2,9 @@ import { version } from '../../package.json';
 import { Router } from 'express';
 import root from './root';
 import TelegramBot from 'node-telegram-bot-api';
-import { extractLink, validURL, sleep } from '../lib/util';
+import { extractLink, validURL, isPhoto } from '../lib/util';
 import download from 'download';
+import _, { size } from 'lodash';
 
 export default () => {
 	let bot = new TelegramBot(process.env.BOT_TOKEN, {polling: true, filepath: false});
@@ -62,11 +63,51 @@ export default () => {
 					// 	console.log('slowing down');
 					// 	sleep(30000);
 					// }
-					bot.sendDocument(chatId, image, image, {}, {
+					bot.sendDocument(chatId, image, {}, {
 						filename: links[i].filename.replace(/\.[^/.]+$/, '')
 					});
 				});
 			}
+		} catch (error) {
+			bot.sendMessage(chatId, error.message);
+		}
+	});
+
+	bot.onText(/\/mini (.+)/, async (msg, match) => {
+		const chatId = msg.chat.id;
+		const resp = match[1];
+
+		try {
+			let isValid = validURL(resp);
+			if (!isValid) {
+				throw Error('Invalid url');
+			}
+			const links = await extractLink(resp);
+			bot.sendMessage(chatId, 'Begin downloading, please be patient');
+			for (const i in links) {
+				links[i].type = isPhoto ? 'photo' : 'video';
+			}
+			let media = {
+				data: _.chunk(links, 10)
+			};
+			for(const i in media.data) {
+				bot.sendMediaGroup(chatId, media.data[i]);
+			}
+			
+		} catch (error) {
+			bot.sendMessage(chatId, error.message);
+		}
+	});
+
+	bot.onText(/\/help/, async (msg) => {
+		const chatId = msg.chat.id;
+		try {
+			bot.sendMessage(chatId, `Commands: 
+			/get <url> - Max album size 20 images.
+			/getp <url> -  No Max album size.
+			/mini <url> - Compressed grouped images. Buggy
+			`);
+						
 		} catch (error) {
 			bot.sendMessage(chatId, error.message);
 		}
--- src/lib/util.js
+++ src/lib/util.js
@@ -27,13 +27,18 @@ export function toRes(res, status=200) {
 }
 
 export function validURL(str) {
-  var pattern = new RegExp('^(https?:\\/\\/)?'+ // protocol
+  let pattern = new RegExp('^(https?:\\/\\/)?'+ // protocol
     '(((cyberdrop*)\\.)+[a-z]{2,}|'+ // domain name
     '((\\d{1,3}\\.){3}\\d{1,3}))'+ // OR ip (v4) address
     '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*'+ // port and path
     '(\\?[;&a-z\\d%_.~+=-]*)?'+ // query string
     '(\\#[-a-z\\d_]*)?$','i'); // fragment locator
   return !!pattern.test(str);
+}
+
+export function isPhoto(str) {
+	let pattern	= new RegExp('/\.(gif|jpe?g|tiff?|png|webp|bmp|mp4)$/i');
+	!!pattern.test(str);
 }
 
 export function extractLink(url) {
